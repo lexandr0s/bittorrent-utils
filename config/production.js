391daa35bcad1233c5e8c4e7a1358c2e80544d16
Yaroslav Sorochan <yaroslav.sorochan.i@gmail.com>
2021-10-15 16:05:30 +0300

    dev

--- config/production.js
+++ config/production.js
@@ -43,6 +43,7 @@ module.exports = {
         seeds_prioritized: false,
         dir_torrent_files_flag: true,
         dir_torrent_files: 'torrents',
+        start_minimized: false,
 	    'rss.update_interval': 1,
         'offers.sponsored_torrent_offer_enabled': false,
         'offers.left_rail_offer_enabled': false
